d3a79bac185980c8364e087526fe9db075d8fb86
Laxmikant Kale <kale@Sanjays-MacBook-Pro14-5.local>
2023-05-28 21:54:38 -0500

    first complet version.. Still has some issues But it at least completes.

--- charm++/tasksWithRing/tasksAndRing.C
+++ charm++/tasksWithRing/tasksAndRing.C
@@ -1,16 +1,11 @@
 #include <stdio.h>
-#include "hello.decl.h"
+#include "tasksAndRing.decl.h"
 
-#include <syncCB.h>
+#include "syncCB.h"
 
-/* readonly */ int tasksPerChare = 4;
 /*readonly*/ CProxy_Main mainProxy;
 /*readonly*/ int numChares;
-/* readonly*/ CProxy_NodeTasks nodeMgr;
-/* readonly */ double baseLoad;  // both input in integer microseconds
-/* readonly */ double maxLoad;  
-/* readonly */ int taskEventID = 0;
-
+/* readonly */ int fibmax = 15; 
 int taskHandlerIndex = 0;
 // one of exceptions.. this is not readonly, but meant for the converse functions, not Charm++, so it is ok. 
 
@@ -18,38 +13,26 @@ int taskHandlerIndex = 0;
 class Main : public CBase_Main
 {
   Main_SDAG_CODE
-  CProxy_Hello taskRunner;
+  CProxy_TasksAndRing taskRunner;
   double t0, t1, t2, t3; 
  public:
   Main(CkArgMsg* m)
   {
-    if(m->argc == 4 ) {     //Process command-line arguments
-	baseLoad=0.000001* atoi(m->argv[1]);
-	maxLoad = 0.000001 *atoi(m->argv[2]);
-	tasksPerChare = atoi(m->argv[3]);
+    ckout <<"starting" <<endl;
+    if(m->argc == 2 ) {     //Process command-line arguments
+	fibmax = atoi(m->argv[1]);
     }
-    else CkAbort("3 arguments needed: baseLoad, maxLoad, tasksPerChare\n");
+    else CkAbort("1 argument needed: fibmax: each chare fires fibmax recursive tasks\n");
 
-    ckout << "baseload: " << baseLoad << "maxLoad: " << maxLoad << ", tasksPerChare:" << tasksPerChare << endl;
+    ckout << "fibmax " << fibmax  << endl;
     delete m;
-    taskEventID = traceRegisterUserEvent("Task execution"); // for projections visualization
     numChares= 2* CkNumPes(); // 2 chares on each PE. 
     mainProxy = thisProxy;
     taskRunner = CProxy_TasksAndRing::ckNew(numChares);
+    taskRunner.startTasks();
   };
  };
 
-
-// the dummy TASK...
-float doWork(int j) {
- // do dummy work proportional to j microseconds, between baseLoad and maxLoad;
-  float y = 1.0;
-  float duration = baseLoad + (maxLoad - baseLoad)*j/(tasksPerChare*numChares);
-  double t0 = CkTimer();
-  while ((CkTimer() - t0) < duration)  y += 1.0 + 1/y; // just a dyummy calculation
-  // calculate and return y, so the compiler doesn't optimize it away.
-  return y;
-}
 // *****************************************************************************************
 //                                          TaskQ method
 
@@ -63,32 +46,27 @@ struct ConverseTaskMsg {
 void taskHandler(void *m1)
 {
   ConverseTaskMsg * m = (ConverseTaskMsg *) m1;
-  //ckout << "in taskHandler" << m->loadLow << ":" << m->loadHigh << endl;
-  //  ckout << m->sync->cb << endl; can we print a callback?
+  ckout << "in taskHandler"  << m->num << endl;
   if (m->num <2 ) // There is only 1 task. Do it.
     {
-      traceBeginUserBracketEvent(taskEventID);
-      doWork(m->loadLow);
-      traceEndUserBracketEvent(taskEventID);
       SyncCB * s =  m->sync;
       // later.. if i == 1, increment the number whose address is provided in the message. 
       s->decrement(); // decrement atomically, and if it becomes 0, send callback to the originator chare.
       CmiFree(m);
     }
   else
-    { // fire 2 tasks with the range divided approximately equally
-      // loadLow:mid, mid+1:loadHigh. Reuse 1 message.
-      int n1 = num-1; int num2 = num -2;
+    { // fire 2 tasks for m->num -1 and m->num - 2 
+      ckout << "i the else clause to fire recursive subtasks" << endl;
       ConverseTaskMsg * m2 = (ConverseTaskMsg *) CmiAlloc(sizeof(ConverseTaskMsg)) ;
       m2->num = m->num - 2;
       m2->sync = m->sync;
       m2->sync->increment(1);
       CmiSetHandler(m2, taskHandlerIndex);
-      CsdTaskEnqueue(m2);
+      CsdTaskEnqueue(m2); // ***** fire task for num-2
 
       m->num--;
       //   m->sync->decrement(); m->sync->increment(1); cancel out. 
-      CsdTaskEnqueue(m);
+      CsdTaskEnqueue(m); // ****** fire task for num-1
     }
 }
 
@@ -109,18 +87,18 @@ class TasksAndRing : public CBase_TasksAndRing
   int count;
   int nextIndex;
   SyncCB * s;
- TaskAndRing()
+  int allTasksDone = 0;
+  TasksAndRing()
   {
     CkPrintf("[%d] Client %d created\n", CkMyPe(), thisIndex);
     count = 0;
     allTasksDone = 0;
     nextIndex = thisIndex +1;
     if (nextIndex >= numChares) nextIndex = 0;
-    thisProxy[nextIndex].forwardRing();
+    if (0 == thisIndex) thisProxy[nextIndex].forwardRing();
   }
 
-  TaskAndRing(CkMigrateMessage *m) {}
-
+  TasksAndRing(CkMigrateMessage *m) {}
   // create tasks that recursively compute first FIBMAX Fibonacci/Pingala numbers
   // while concurrently serving a ring message going around the chare array
   // the ring message should be higher priority above tasks. So, tasks (beyond the currently running one)
@@ -129,30 +107,40 @@ class TasksAndRing : public CBase_TasksAndRing
   void forwardRing()
   {
     count++;
-    if ( (0 == thisindex) &&  (allTasksDone) ) 
-      {
-	ckout << " numer of rings completed is : " << count << endl;
+    ckout << thisIndex <<  ": in forwardring " << ":count=" << count << endl;
+    if ( (0 == thisIndex) &&  (allTasksDone==1) ) 
+      {	ckout << thisIndex << ": numer of rings completed is : " << count << "allTasksDone:"<< allTasksDone << endl;
 	CkExit();
       }
     else
+      { ckout << thisIndex << ": forwarding to " << nextIndex << endl;
       thisProxy[nextIndex].forwardRing();
+      }
   }    
 
  void done() {
+   ckout << thisIndex << ":all tasks done." << endl;
    allTasksDone = 1;
   }
 
  void startTasks()
  { // for now, just do the work inline
-   int r = thisIndex; 
-   int k = tasksPerChare; // should be a command line para. Each chare creates 4 (or k) tasks
-    for (int i=0; i<k; i++) 
-      doWork(k*r+i);
-    CkCallback cb(CkReductionTarget(Main, done), mainProxy);
-    contribute(0, NULL, CkReduction::nop, cb);    
+   int r = thisIndex;
+   CkCallback cb(CkIndex_TasksAndRing::tasksCompleted() , thisProxy[thisIndex]);
+   s = new SyncCB(cb,fibmax);
+   for (int i=0; i<fibmax; i++) 
+     {
+      ConverseTaskMsg * m2 = (ConverseTaskMsg *) CmiAlloc(sizeof(ConverseTaskMsg)) ;
+      m2->num = i;
+      m2->sync = s ;
+      CmiSetHandler(m2, taskHandlerIndex);
+      CsdTaskEnqueue(m2); // ***** fire task
+     }   
+   ckout << thisIndex << ": in startTasks. started them" << endl;
+
  }
 
-// *****************************************************************************************
+ /*
   void fireViaTaskQ()
   {
     CkCallback cb(CkIndex_TaskAndRing::taskQTasksCompleted() , thisProxy[thisIndex]);
@@ -166,13 +154,14 @@ class TasksAndRing : public CBase_TasksAndRing
     // firing 1 task, which will be recursivelu subdivided to tasksPerchare real tasks
     CsdTaskEnqueue(msg);
   }
-
-  void taskQTasksCompleted()
+ */
+  void tasksCompleted()
   {
+    ckout << thisIndex << ": in tasksCompled " << endl;
     delete s;
-    CkCallback cb(CkReductionTarget(Main, done), mainProxy);
+    CkCallback cb(CkReductionTarget(TasksAndRing, done), thisProxy[0]);
     contribute(0, NULL, CkReduction::nop, cb);    
   }
 };
 
-#include "hello.def.h"
+#include "tasksAndRing.def.h"
