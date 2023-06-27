31532de07d26f33abb5805f06c0835e49f844f58
Kavitha Chandrasekar <kchndrs2@illinois.edu>
2023-06-27 17:00:40 -0500

    Add timers, print on pe-0

--- converse/ping_ack/bigmsg.c
+++ converse/ping_ack/bigmsg.c
@@ -6,6 +6,9 @@
 CpvDeclare(int, bigmsg_index);
 CpvDeclare(int, recv_count);
 CpvDeclare(int, ack_count);
+CpvDeclare(double, total_time);
+CpvDeclare(double, process_time);
+CpvDeclare(double, send_time);
 
 #define MSG_SIZE 8
 #define MSG_COUNT 100
@@ -46,7 +49,9 @@ void bigmsg_handler(void *vmsg)
   } else { //Pe-0 receives all acks
     CpvAccess(ack_count) = 1 + CpvAccess(ack_count);
     if(CpvAccess(ack_count) == CmiNumPes()/2) {
-      CmiPrintf("\nReceived ack on PE-#%d", CmiMyPe());
+      CpvAccess(total_time) = CmiWallTimer() - CpvAccess(total_time);
+      CmiPrintf("\nReceived ack on PE-#%d send time=%lf, process time=%lf, total time=%lf",
+              CmiMyPe(), CpvAccess(send_time), CpvAccess(process_time), CpvAccess(total_time));
       CmiFree(msg);
       Cpm_bigmsg_stop(CpmSend(CpmALL));
     }
@@ -56,6 +61,7 @@ void bigmsg_handler(void *vmsg)
 void bigmsg_init()
 {
   int i, k;
+  double start_time, crt_time;
   struct myMsg *msg;
   int totalpes = CmiNumPes(); //p=num_pes
   int pes_per_node = totalpes/2; //q=p/2
@@ -68,12 +74,15 @@ void bigmsg_init()
     if(CmiMyPe() < pes_per_node) {
       CmiPrintf("\nSending msg fron pe%d to pe%d\n",CmiMyPe(), CmiMyNodeSize()+CmiMyRank());
       for(k=0;k<MSG_COUNT;k++) {
-//        CmiPrintf("\nSending msg number #%d\n", k);
+        crt_time = CmiWallTimer();
         msg = (message)CmiAlloc(sizeof(struct myMsg));
         for (i=0; i<MSG_SIZE; i++) msg->payload[i] = i;
         CmiSetHandler(msg, CpvAccess(bigmsg_index));
+        CpvAccess(process_time) = CmiWallTimer() - crt_time + CpvAccess(process_time);
+        start_time = CmiWallTimer();
         //Send from my pe-i on node-0 to q+i on node-1
         CmiSyncSendAndFree(pes_per_node+CmiMyPe(), sizeof(struct myMsg), msg);
+        CpvAccess(send_time) = CmiWallTimer() - start_time + CpvAccess(send_time);
       }
     }
   }
@@ -84,6 +93,10 @@ void bigmsg_moduleinit(int argc, char **argv)
   CpvInitialize(int, bigmsg_index);
   CpvInitialize(int, recv_count);
   CpvInitialize(int, ack_count);
+  CpvInitialize(double, total_time);
+  CpvInitialize(double, send_time);
+  CpvInitialize(double, process_time);
+
   CpvAccess(bigmsg_index) = CmiRegisterHandler(bigmsg_handler);
   void CpmModuleInit(void);
   void CfutureModuleInit(void);
